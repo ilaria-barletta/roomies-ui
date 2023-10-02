99554ca0218ebbde62747178e955c8dfe8ed9416
ilaria-barletta <114695123+ilaria-barletta@users.noreply.github.com>
2023-10-02 19:38:52 +0200

    add sign in form component and page

---
+++ src/pages/auth/SignInForm.js
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+
+import { Form, Button, Col, Row, Container } from "react-bootstrap";
+import axios from "axios";
+
+const SignInForm = () => {
+  const [signInData, setSignInData] = useState({
+    username: "",
+    password: "",
+  });
+  const { username, password } = signInData;
+
+  const history = useHistory();
+
+  const handleChange = (event) => {
+    setSignInData({
+      ...signInData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axios.post("/dj-rest-auth/login/", signInData);
+      history.push("/");
+    } catch (err) {
+      // TODO: Add errors to top of the form
+    }
+  };
+
+  return (
+    <Row>
+      <Col className="my-auto py-2 p-md-2" md={6}>
+        <Container className="p-4">
+          <h1>Sign In</h1>
+
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="username">
+              <Form.Label className="d-none">Username</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Username"
+                name="username"
+                value={username}
+                onChange={handleChange}
+              />
+            </Form.Group>
+
+            <Form.Group controlId="password1">
+              <Form.Label className="d-none">Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                name="password"
+                value={password}
+                onChange={handleChange}
+              />
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+              Sign in
+            </Button>
+          </Form>
+        </Container>
+
+        <Container className="mt-3">
+          <Link to="/signup">
+            Don't have an account? <span>Sign up</span>
+          </Link>
+        </Container>
+      </Col>
+    </Row>
+  );
+};
+
+export default SignInForm;
