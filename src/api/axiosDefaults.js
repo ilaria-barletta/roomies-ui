887265604dcfbc798a4aa7d0a7dbd63bb1585ac1
ilaria-barletta <114695123+ilaria-barletta@users.noreply.github.com>
2023-10-02 12:32:33 +0200

    add signin form and page

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,4 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "https://roomiesapi-637170cefd22.herokuapp.com/";
+axios.defaults.withCredentials = true;
---
+++ src/pages/auth/SignUpForm.js
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+
+import { Form, Button, Col, Row, Container } from "react-bootstrap";
+import axios from "axios";
+
+const SignUpForm = () => {
+  const [signUpData, setSignUpData] = useState({
+    username: "",
+    password1: "",
+    password2: "",
+  });
+  const { username, password1, password2 } = signUpData;
+
+  const history = useHistory();
+
+  const handleChange = (event) => {
+    setSignUpData({
+      ...signUpData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axios.post("/dj-rest-auth/registration/", signUpData);
+      history.push("/signin");
+    } catch (err) {
+      // TODO: Add errors to top of the form
+    }
+  };
+
+  return (
+    <Row>
+      <Col className="my-auto py-2 p-md-2" md={6}>
+        <Container className="p-4">
+          <h1>Create an account</h1>
+
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="username">
+              <Form.Label className="d-none">Username</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Username"
+                name="username"
+                value={username}
+                onChange={handleChange}
+              />
+            </Form.Group>
+
+            <Form.Group controlId="password1">
+              <Form.Label className="d-none">Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                name="password1"
+                value={password1}
+                onChange={handleChange}
+              />
+            </Form.Group>
+
+            <Form.Group controlId="password2">
+              <Form.Label className="d-none">Confirm password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Confirm password"
+                name="password2"
+                value={password2}
+                onChange={handleChange}
+              />
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+              Sign up
+            </Button>
+          </Form>
+        </Container>
+
+        <Container className="mt-3">
+          <Link to="/signin">
+            Already have an account? <span>Sign in</span>
+          </Link>
+        </Container>
+      </Col>
+    </Row>
+  );
+};
+
+export default SignUpForm;
