f50a4d62950cb5bbe12883380cf14a461ecc4358
ilaria-barletta <114695123+ilaria-barletta@users.noreply.github.com>
2023-10-03 11:03:31 +0200

    add button to view households and go back to the list

--- src/pages/allhouseholds/AllHouseholds.js
+++ src/pages/allhouseholds/AllHouseholds.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Card from "react-bootstrap/Card";
-import { Button } from "react-bootstrap";
+import { Button, Container } from "react-bootstrap";
+import HouseholdDetails from "../../components/households/HouseholdDetails";
 
 const AllHouseholds = () => {
   const [households, setHouseHolds] = useState(null);
+  const [currentHouseholdDetails, setCurrentHouseholdDetails] = useState(null);
 
   const loadHouseholds = async () => {
     const { data } = await axios.get("/households/");
@@ -15,8 +17,29 @@ const AllHouseholds = () => {
     loadHouseholds();
   }, []);
 
+  const viewHouseholdDetails = (household) => {
+    setCurrentHouseholdDetails(household);
+  };
+
+  const viewAllHouseholds = () => {
+    setCurrentHouseholdDetails(null);
+  };
+
   if (!households || !households.length) {
     return <>There are no households to show.</>;
+  }
+
+  if (currentHouseholdDetails) {
+    return (
+      <>
+        <HouseholdDetails household={currentHouseholdDetails} />
+        <Container className="mt-3">
+          <Button variant="link" onClick={viewAllHouseholds}>
+            View all households
+          </Button>
+        </Container>
+      </>
+    );
   }
 
   return (
@@ -25,7 +48,12 @@ const AllHouseholds = () => {
         <Card>
           <Card.Body>
             <Card.Title>{household.name}</Card.Title>
-            <Button variant="primary">View Details</Button>
+            <Button
+              variant="primary"
+              onClick={() => viewHouseholdDetails(household)}
+            >
+              View Details
+            </Button>
           </Card.Body>
         </Card>
       ))}
