6f026fff6c0bbdac9414ba5af91cb4fe78dc50bb
ilaria-barletta <114695123+ilaria-barletta@users.noreply.github.com>
2023-10-12 15:16:12 +0200

    add assignee option to grocery item form

--- src/components/households/groceries/GroceryItemForm.js
+++ src/components/households/groceries/GroceryItemForm.js
@@ -1,16 +1,19 @@
 import axios from "axios";
-import React, { useState } from "react";
-import { Form, Button, Row, Col, Container } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { Form, Button, Row, Col, Container, Spinner } from "react-bootstrap";
 import { useHistory } from "react-router-dom";
 
 const GroceryItemForm = ({ listId, onItemAdded }) => {
   const history = useHistory();
+  const [availableMembers, setAvailableMembers] = useState();
+  const [loading, setIsLoading] = useState(true);
   const [itemData, setItemData] = useState({
     name: "",
+    assignee: "",
     list: listId,
     is_complete: false,
   });
-  const { name } = itemData;
+  const { name, assignee } = itemData;
 
   const handleChange = (event) => {
     setItemData({
@@ -19,20 +22,51 @@ const GroceryItemForm = ({ listId, onItemAdded }) => {
     });
   };
 
+  const loadList = async () => {
+    const { data } = await axios.get(`/grocerylists/${listId}/`);
+    return data;
+  };
+
+  const loadHousehold = async (list) => {
+    const { data } = await axios.get(`/households/${list.household}/`);
+    return data;
+  };
+
+  const loadAvailableMembersData = async () => {
+    const list = await loadList();
+    const household = await loadHousehold(list);
+
+    // The user can choose the household members or creator
+    // to assign to the grocery list item
+    const members = household.members.concat([
+      { user: household.creator_id, user_name: household.creator },
+    ]);
+    setAvailableMembers(members);
+    setIsLoading(false);
+  };
+
   const goBack = () => {
     // Found here: https://v5.reactrouter.com/web/api/history
     history.goBack();
   };
 
+  useEffect(() => {
+    loadAvailableMembersData();
+  }, []);
+
   const handleSubmit = async (event) => {
     event.preventDefault();
     await axios.post("/groceryitems/", itemData);
-    setItemData({
-      ...itemData, // Keep the list ID after resetting the data
-      name: "",
-    });
     onItemAdded();
   };
+
+  if (loading) {
+    return (
+      <Container className="d-flex justify-content-center">
+        <Spinner animation="border" variant="primary" />
+      </Container>
+    );
+  }
 
   return (
     <Row>
@@ -50,6 +84,22 @@ const GroceryItemForm = ({ listId, onItemAdded }) => {
                 value={name}
                 onChange={handleChange}
               />
+            </Form.Group>
+
+            <Form.Group controlId="exampleForm.SelectCustom">
+              <Form.Label>Assigned member</Form.Label>
+              <Form.Control
+                as="select"
+                custom
+                value={assignee}
+                onChange={handleChange}
+                name="assignee"
+              >
+                {availableMembers?.map((member) => (
+                  <option value={member.user}>{member.user_name}</option>
+                ))}
+                <option value="">Select</option>
+              </Form.Control>
             </Form.Group>
 
             <Button variant="primary" type="submit">
