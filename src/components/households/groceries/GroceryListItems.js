ba13302cc2c2624b45b092a73082a3e726b49040
ilaria-barletta <114695123+ilaria-barletta@users.noreply.github.com>
2023-10-13 14:45:49 +0200

    add option to change grocery item completed status

--- src/components/households/groceries/GroceryListItems.js
+++ src/components/households/groceries/GroceryListItems.js
@@ -13,12 +13,19 @@ import {
 const GroceryListItems = ({ listId }) => {
   const [items, setItems] = useState();
   const [showDeleteItemPopup, setShowDeleteItemPopup] = useState(false);
+  const [showItemStatusPopup, setShowItemStatusPopup] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
   const [itemToDelete, setItemToDelete] = useState();
+  const [itemToChangeStatus, setItemToChangeStatus] = useState();
 
   const onCloseDeleteItemPopup = () => {
     setItemToDelete(null);
     setShowDeleteItemPopup(false);
+  };
+
+  const onCloseChangeItemStatusPopup = () => {
+    setItemToChangeStatus(null);
+    setShowItemStatusPopup(false);
   };
 
   const loadItems = async () => {
@@ -39,10 +46,54 @@ const GroceryListItems = ({ listId }) => {
     loadItems();
   };
 
+  const toggleItemCompleted = async () => {
+    // Copy the item data and change the value of the is_complete
+    // field from true -> false or false -> true
+    const itemData = {
+      ...itemToChangeStatus,
+      is_complete: !itemToChangeStatus.is_complete,
+    };
+
+    await axios.put(`/groceryitems/${itemToChangeStatus.id}/`, itemData);
+    setItemToChangeStatus(null);
+    setShowItemStatusPopup(false);
+    loadItems();
+  };
+
   const onClickDeleteItem = (item) => {
     setItemToDelete(item);
     setShowDeleteItemPopup(true);
   };
+
+  const showChangeItemStatusPopup = (item) => {
+    setShowItemStatusPopup(true);
+    setItemToChangeStatus(item);
+  };
+
+  const itemChangeStatusPopup = (
+    <Modal show={showItemStatusPopup} onHide={onCloseChangeItemStatusPopup}>
+      <Modal.Header closeButton>
+        <Modal.Title>
+          {itemToChangeStatus?.is_complete
+            ? "Mark item as incomplete"
+            : "Mark item as complete"}
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        Are you sure you want to{" "}
+        {itemToChangeStatus?.is_complete ? "undo completing" : "complete"}{" "}
+        {itemToChangeStatus?.name}?
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={onCloseChangeItemStatusPopup}>
+          Cancel
+        </Button>
+        <Button variant="primary" onClick={toggleItemCompleted}>
+          Update
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
 
   const confirmDeleteItemPopup = (
     <Modal show={showDeleteItemPopup} onHide={onCloseDeleteItemPopup}>
@@ -77,13 +128,28 @@ const GroceryListItems = ({ listId }) => {
       {items.map((item) => (
         <div className="d-flex justify-content-between mb-2">
           <div>
-            {item.name} <Badge variant="secondary">{item.assignee_name}</Badge>
+            {/** <s> tag will draw a line through completed items text */}
+            {item.is_complete && (
+              <s>
+                {item.name}{" "}
+                <Badge variant="secondary">{item.assignee_name}</Badge>
+              </s>
+            )}
+            {!item.is_complete && (
+              <span>
+                {item.name}{" "}
+                <Badge variant="secondary">{item.assignee_name}</Badge>
+              </span>
+            )}
           </div>
           <DropdownButton
             variant="secondary"
             id="manage-item-button"
             title="Item Actions"
           >
+            <Dropdown.Item onClick={() => showChangeItemStatusPopup(item)}>
+              {item.is_complete ? "Mark as incomplete" : "Mark as complete"}
+            </Dropdown.Item>
             <Dropdown.Item
               href={`/grocerylists/${listId}/items/${item.id}/edit`}
             >
@@ -97,6 +163,7 @@ const GroceryListItems = ({ listId }) => {
       ))}
 
       {confirmDeleteItemPopup}
+      {itemChangeStatusPopup}
     </div>
   );
 };
