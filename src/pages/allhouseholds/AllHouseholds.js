6cc7deca2bdb024b55fde70511f9a1aa4ca933cc
ilaria-barletta <114695123+ilaria-barletta@users.noreply.github.com>
2023-10-17 17:17:22 +0200

    add edit and delete buttons to households list

--- src/pages/allhouseholds/AllHouseholds.js
+++ src/pages/allhouseholds/AllHouseholds.js
@@ -2,14 +2,63 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import Card from "react-bootstrap/Card";
+import { toast } from "react-toastify";
 import Spinner from "react-bootstrap/Spinner";
+import Modal from "react-bootstrap/Modal";
 import { Button, Container } from "react-bootstrap";
 import HouseholdDetails from "../../components/households/HouseholdDetails";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 const AllHouseholds = () => {
   const [households, setHouseHolds] = useState(null);
   const [currentHouseholdDetails, setCurrentHouseholdDetails] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const currentUser = useCurrentUser();
+  const [showDeletePopup, setShowDeletePopup] = useState(false);
+  const [householdToDelete, setHouseholdToDelete] = useState();
+
+  const onCloseDeletePopup = () => {
+    setShowDeletePopup(false);
+    setHouseholdToDelete(null);
+  };
+
+  const onShowDeletePopup = (household) => {
+    setShowDeletePopup(true);
+    setHouseholdToDelete(household);
+  };
+
+  // Validate that the owner is the only one that can do household actions
+  const isOwner = (household) => currentUser?.username === household.creator;
+
+  const deleteHousehold = async () => {
+    try {
+      await axios.delete(`/households/${householdToDelete.id}/`);
+      setShowDeletePopup(false);
+      toast.success("Successfully deleted the household.");
+      householdDeleted();
+    } catch {
+      toast.error("Failed to delete the household. Please try again.");
+    }
+  };
+
+  const confirmDeletePopup = (
+    <Modal show={showDeletePopup} onHide={onCloseDeletePopup}>
+      <Modal.Header closeButton>
+        <Modal.Title>Delete Household</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        Are you sure you want to delete {householdToDelete?.name}?
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={onCloseDeletePopup}>
+          Cancel
+        </Button>
+        <Button variant="danger" onClick={deleteHousehold}>
+          Delete
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
 
   const loadHouseholds = async () => {
     const { data } = await axios.get("/households/");
@@ -72,18 +121,33 @@ const AllHouseholds = () => {
         </Link>
       </div>
       {households.map((household) => (
-        <Card>
+        <Card className="mb-3">
           <Card.Body>
             <Card.Title>{household.name}</Card.Title>
             <Button
               variant="primary"
+              className="mr-1"
               onClick={() => viewHouseholdDetails(household)}
             >
               View Details
             </Button>
+            {isOwner(household) && (
+              <>
+                <Link to={`/households/${household.id}/edit`} className="mr-1">
+                  <Button variant="secondary">Edit</Button>
+                </Link>
+                <Button
+                  variant="danger"
+                  onClick={() => onShowDeletePopup(household)}
+                >
+                  <i className="fas fa-trash-alt" />
+                </Button>
+              </>
+            )}
           </Card.Body>
         </Card>
       ))}
+      {confirmDeletePopup}
     </>
   );
 };
