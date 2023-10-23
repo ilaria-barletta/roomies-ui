cd24bfb35780e95e68ac45704a61d6bb85472a63
ilaria-barletta <114695123+ilaria-barletta@users.noreply.github.com>
2023-10-23 14:26:36 +0100

    style the welcome page

--- src/pages/welcome/WelcomePage.js
+++ src/pages/welcome/WelcomePage.js
@@ -2,6 +2,8 @@ import React, { useEffect } from "react";
 import { Link, useHistory } from "react-router-dom";
 import { Button } from "react-bootstrap";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import logo from "./logo.png";
+import styles from "../../styles/WelcomePage.module.css";
 
 const WelcomePage = () => {
   const currentUser = useCurrentUser();
@@ -16,13 +18,23 @@ const WelcomePage = () => {
 
   return (
     <>
-      <h1 className="mb-3">Welcome to Roomies!</h1>
-      <Link to="/signin" className="mr-1">
-        <Button>Sign In</Button>
-      </Link>
-      <Link to="/signup">
-        <Button>Sign Up</Button>
-      </Link>
+      <h1 className="mb-3 text-center">Roomies</h1>
+      <div className="mb-3 d-flex justify-content-center">
+        <img src={logo} alt="Roomies logo" className={styles.WelcomeImage} />
+      </div>
+      <p className="mb-3">
+        Manage your households in an easy way! Add roomies, create grocery lists
+        and track your rent payments all in one place. They say sharing isn't
+        easy..we say it doesn't have to be hard
+      </p>
+      <div className="d-flex justify-content-center">
+        <Link to="/signin" className="mr-1">
+          <Button>Sign In</Button>
+        </Link>
+        <Link to="/signup">
+          <Button>Sign Up</Button>
+        </Link>
+      </div>
     </>
   );
 };
